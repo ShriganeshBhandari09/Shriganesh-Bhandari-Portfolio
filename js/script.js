da7a771db47b6be20cbb8f21db0751c7059b6e5c
Shriganesh Bhandari <shriganesh.bhandari693@gmail.com>
2025-01-29 21:27:23 +0530

    Updated Mobile Navbar

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const navigationButton = document.getElementById("navigation-button");
 const navbarMenu = document.getElementById("navbar-menu");
 
+const navlinks = document.querySelectorAll(".nav-links");
+
 function displayNavigationMenu() {
   if (navbarMenu.classList == "navbar-active") {
     navbarMenu.classList.remove("navbar-active");
@@ -14,6 +16,17 @@ function displayNavigationMenu() {
     navigationButton.classList.add("fa-xmark");
   }
 }
+
+function closeNavbar() {
+  navbarMenu.classList.remove("navbar-active");
+  navbarMenu.classList.add("navbar-deactivate");
+  navigationButton.classList.remove("fa-xmark");
+  navigationButton.classList.add("fa-bars");
+}
+
+navlinks.forEach((link) => {
+  link.addEventListener("click", closeNavbar);
+});
 
 function initializeNavbar() {
   if (window.matchMedia("(min-width: 992px)").matches) {
